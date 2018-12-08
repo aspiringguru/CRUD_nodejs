aa64778ebf04549218cae7351e63e800b0d51f28
aspiringguru <bmatthewtaylor@gmail.com>
2018-12-08 19:38:50 +1000

    Models, Controllers, Views and Routes structure added

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const product = require('./routes/product.route');
+// Imports routes for the products
+
+
 // initialize our express app
 const app = express();
+
+app.use('/products', product);
 
 let port = 1234;
 
